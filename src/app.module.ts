a034fd08ae8cffc3867ef37fc33310d7121458b0
eduardo678u <carrilloandres179@gmail.com>
2024-09-09 18:23:25 -0500

    se agrega proyecto

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Client } from './clients/entities/client.entity';
+
+@Module({
+  imports: [ClientsModule, 
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '10018913355',
+      database: 'Culbd2',
+      entities: [
+        User,
+        Client
+      ],
+      synchronize: true,
+    }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/clients/entities/client.entity.ts
@@ -0,0 +1,7 @@
+import { Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Client {
+    @PrimaryGeneratedColumn()
+    id: number;
+}
